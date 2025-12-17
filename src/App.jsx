199ff289613b3c8e7103ca0cef46b9b58cd1d42c
Jeyakumarthomas <jeyakumarthomas23123@example.com>
2025-12-17 17:53:12 +0530

    Initialize safari window component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Draggable } from "gsap/Draggable";
 gsap.registerPlugin(Draggable);
 
 import { Navbar,Welcome,Dock } from '#components';
-import { Terminal } from '#windows';
+import { Terminal,Safari } from '#windows';
 
 
 const App = () => {
@@ -14,6 +14,7 @@ const App = () => {
         <Welcome/>
         <Dock/>
         <Terminal/>
+        <Safari/>
     </main>
 
   )
